1388e9d822db1eb3b06f3e27abc58a2558347bfb
Jing Qiang <tohjingqiang@gmail.com>
2023-07-12 22:47:35 -0700

    add dashboard page with chatbot card (ui)

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,17 +3,32 @@ import React from "react";
 interface ButtonProps {
   children: React.ReactNode;
   className?: string;
+  onClick?: () => void;
+  variant?: "solid" | "ghost";
 }
 
 export const Button = (props: ButtonProps) => {
-  const { children, className } = props;
+  const { children, className, onClick, variant = "solid" } = props;
+
+  let variantButtonClassName = "";
+  switch (variant) {
+    case "solid":
+      variantButtonClassName =
+        "bg-brand-sunglow hover:bg-brand-sandy-brown text-brand-gunmetal";
+      break;
+    case "ghost":
+      variantButtonClassName =
+        "bg-white border border-brand-sunglow text-brand-sunglow hover:bg-brand-sunglow hover:text-brand-gunmetal";
+      break;
+  }
 
   return (
     <div
       className={
-        "px-[20px] md:px-[22px] py-[2px] md:py-[4px] bg-brand-sunglow hover:bg-brand-sandy-brown text-brand-gunmetal font-semibold rounded-3xl" +
-        ` ${className}`
+        "px-[20px] md:px-[22px] py-[2px] md:py-[4px] cursor-pointer font-semibold rounded-3xl" +
+        ` ${variantButtonClassName} ${className}`
       }
+      onClick={onClick}
     >
       {children}
     </div>
---
+++ src/components/ChatbotCard.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { Button } from "./Button";
+import toast from "react-hot-toast";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import { ButtonLink } from "./ButtonLink";
+
+export interface ChatbotCardProps {
+  chatbotId: string;
+  name: string;
+  trainedData: string;
+  parameters: {
+    personality: string;
+    language: string;
+  };
+  usage: {
+    currentApiRequests: number;
+    maxApiRequests: number;
+    timeUsed: number;
+    totalTime: number;
+  };
+}
+
+export const ChatbotCard = (props: ChatbotCardProps) => {
+  const { chatbotId, name, trainedData, parameters, usage } = props;
+
+  const usageApiRequestPercentage =
+    Math.round((usage.currentApiRequests / usage.maxApiRequests) * 100 * 100) /
+    100;
+  const usageTimeUsedPercentage =
+    Math.round((usage.timeUsed / usage.totalTime) * 100 * 100) / 100;
+  const percentageToColorClassName = (percentage: number) => {
+    if (percentage < 50) return "text-green-500";
+    if (percentage < 75) return "text-yellow-500";
+    return "text-red-500";
+  };
+
+  return (
+    <CardContainer>
+      <div className="flex flex-col gap-4">
+        {/* Chatbot Name */}
+        <div className="flex flex-row gap-2 items-center font-semibold text-[24px]">
+          {name}{" "}
+          <a href={`chat/${chatbotId}`}>
+            <FaArrowUpRightFromSquare className="w-[16px] cursor-pointer hover:text-brand-sandy-brown" />
+          </a>
+        </div>
+
+        {/* Trained Data */}
+        <div>
+          Trained Data:{" "}
+          <span className="text-blue-600 underline">{trainedData}</span>
+        </div>
+
+        {/* Tuned Parameters */}
+        <section className="flex flex-col">
+          <span>Tuned Parameters:</span>
+          <div className="flex flex-col text-[#6B6B6B] text-[14px]">
+            <span>{`> Personality: ${parameters.personality}`}</span>
+            <span>{`> Language: ${parameters.language}`}</span>
+          </div>
+        </section>
+
+        {/* Usage */}
+        <section className="flex flex-col">
+          <span>Usage:</span>
+          <div className="flex flex-col text-[#6B6B6B] text-[14px]">
+            <span>
+              {"> API Requests: "}
+              <span
+                className={percentageToColorClassName(
+                  usageApiRequestPercentage
+                )}
+              >
+                {`${usage.currentApiRequests}/${usage.maxApiRequests} (${usageApiRequestPercentage}%)`}
+              </span>
+            </span>
+            <span>
+              {"> Time (minutes): "}
+              <span
+                className={percentageToColorClassName(usageTimeUsedPercentage)}
+              >
+                {`${usage.timeUsed}/${usage.totalTime} (${usageTimeUsedPercentage}%)`}
+              </span>
+            </span>
+          </div>
+        </section>
+
+        {/* Button */}
+        <section className="mt-3 flex gap-2 justify-end">
+          <Button
+            variant="ghost"
+            onClick={() => toast("Edit chatbot feature coming soon...")}
+          >
+            Edit
+          </Button>
+          <ButtonLink href={`chat/${chatbotId}`}>Run</ButtonLink>
+        </section>
+      </div>
+    </CardContainer>
+  );
+};
+
+interface CardContainerProps {
+  children: React.ReactNode;
+}
+
+const CardContainer = (props: CardContainerProps) => {
+  const { children } = props;
+
+  return (
+    <div className="shadow-md hover:shadow-lg px-[22px] py-[16px] min-w-[350px]">
+      {children}
+    </div>
+  );
+};
