032390e22101bfde3fd2d1c31459c157ef54ec50
estherteo <104299126+estherteo@users.noreply.github.com>
2023-07-18 16:20:01 +0800

    Testing drag and drop

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,24 +1,107 @@
-import React, { useState, ChangeEvent } from "react";
+import React, {
+  useState,
+  ChangeEvent,
+  useRef,
+  useEffect,
+  useCallback,
+} from "react";
 import FileUploadIcon from "./FileUploadIcon";
 import { FileIcon, defaultStyles } from "react-file-icon";
+import { useDropzone } from "react-dropzone";
 
+// const FileUploader = (props: any) => {
+//   const { handlePDFFile,handleDragOver ,handleDrop} = props;
+
+//   return (
+//     <div className="dropzone" onDragOver={handleDragOver} onDrop={handleDrop}>
+//       <div className="flex justify-center">
+//         <div className="max-w-xl">
+//           <label className="flex justify-center w-[300px] h-[220px] md:w-[600px] md:h-[320px] px-4 transition bg-[#F4A261]/[0.33] border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-[#F4A261] focus:outline-none my-[10px]">
+//             <span className="flex items-center space-x-2">
+//               <FileUploadIcon className=" h-[15px] w-[15px] md:h-[20px] md:w-[20px] " />
+//               <span className="font-medium text-gray-600 text-[15px] md:text-[20px]">
+//                 Drop files to Attach, or{" "}
+//                 <span className="text-blue-600 text-[15px] md:text-[20px] underline">
+//                   browse
+//                 </span>
+//               </span>
+//             </span>
+//             <input
+//               type="file"
+//               name="file_upload"
+//               className="hidden"
+//               onChange={handlePDFFile}
+//             />
+//           </label>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+//testing
 const FileUploader = (props: any) => {
-  const { handlePDFFile } = props;
-  return (
-    <div className="flex justify-center">
-    <div className="max-w-xl">
-      <label className="flex justify-center w-[300px] h-[220px] md:w-[600px] md:h-[320px] px-4 transition bg-[#F4A261]/[0.33] border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-[#F4A261] focus:outline-none my-[10px]">
-        <span className="flex items-center space-x-2">
-          <FileUploadIcon className=" h-[15px] w-[15px] md:h-[20px] md:w-[20px] "/>
-          <span className="font-medium text-gray-600 text-[15px] md:text-[20px]">
-            Drop files to Attach, or{" "}
-            <span className="text-blue-600 text-[15px] md:text-[20px] underline">browse</span>
-          </span>
-        </span>
-        <input type="file" name="file_upload" className="hidden" onChange={handlePDFFile}/>
-      </label>
+  const { handlePDFFile, handleDragOver, handleDrop } = props;
 
-    </div>
+  useEffect(() => {
+    const preventDefault = (event: any) => {
+      event.preventDefault();
+    };
+    window.addEventListener("dragover", preventDefault);
+    window.addEventListener("drop", preventDefault);
+
+    return () => {
+      window.removeEventListener("dragover", preventDefault);
+      window.removeEventListener("drop", preventDefault);
+    };
+  }, []);
+
+  const onDrop = useCallback(
+    (event: React.DragEvent<HTMLDivElement>) => {
+      event.preventDefault();
+      console.log(event);
+      event.stopPropagation();
+      handleDrop(event);
+    },
+    [handleDrop]
+  );
+
+  const onDragOver = useCallback(
+    (event: React.DragEvent<HTMLDivElement>) => {
+      event.preventDefault(); // Prevent default behavior (e.g., opening the file)
+      // Access event object properties if needed
+      const { dataTransfer } = event;
+      // ...
+    },
+    [handleDragOver]
+  );
+
+  return (
+    <div className="dropzone" onDragOver={handleDragOver} onDrop={onDrop}>
+      <div className="flex justify-center">
+        <div className="max-w-xl">
+          <label className="flex justify-center w-[300px] h-[220px] md:w-[600px] md:h-[320px] px-4 transition bg-[#F4A261]/[0.33] border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-[#F4A261] focus:outline-none my-[10px]">
+            <span className="flex items-center space-x-2">
+              <FileUploadIcon className=" h-[15px] w-[15px] md:h-[20px] md:w-[20px] " />
+              <span className="font-medium text-gray-600 text-[15px] md:text-[20px]">
+                Drop files to Attach, or{" "}
+                <span className="text-blue-600 text-[15px] md:text-[20px] underline">
+                  browse
+                </span>
+              </span>
+            </span>
+            <input
+              type="file"
+              name="file_upload"
+              className="hidden"
+              onChange={(event) => {
+                event.persist();
+                handlePDFFile(event);
+              }}
+            />
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/ChatbotPage.tsx
+++ src/pages/ChatbotPage.tsx
@@ -1,6 +1,6 @@
 import FileUploader from "components/FileUploader";
 import { TextLinearGradient } from "components/Typography/TextLinearGradient";
-import React, { useState } from "react";
+import React, { DragEvent, useState, useRef } from "react";
 import { Viewer } from "@react-pdf-viewer/core"; // install this library
 // Plugins
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout"; // install this library
@@ -10,11 +10,184 @@ import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 // Worker
 import { Worker } from "@react-pdf-viewer/core"; // install this library
 import ChatbotTraining from "components/Modal/ChatbotTraining";
-import ChatbotCreated from "components/Modal/ChatbotCreated";
-import Spinner from "components/Spinner";
-import axios from "axios";
 import { FileIcon, defaultStyles } from "react-file-icon";
 
+// export const ChatbotPage = () => {
+//   // create new plugin instance
+//   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+//   //check if its pdf file
+//   const [pdfFile, setPdfFile] = useState(null);
+//   const [pdfFileError, setPdfFileError] = useState("");
+
+//   // view PDF
+//   const [viewPdf, setViewPdf] = useState(null);
+
+//   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+//   const fileType = ["application/pdf"];
+
+//   // for drag and drop functionality
+//   // const handleDragOver = (event: any) => {
+//   //   event.preventDefault();
+//   // };
+
+//   // const handleDrop = (event: any) => {
+//   //   event.preventDefault();
+//   //   handlePDFFile(event.dataTransfer.files[0]);
+//   // };
+
+//   const handlePDFFile = (event: any) => {
+//     event.preventDefault();
+//     let selectedFile = event.target.files && event.target.files[0];
+//     // if the person has even selected a file
+//     if (selectedFile) {
+//       if (selectedFile && fileType.includes(selectedFile.type)) {
+//         let reader = new FileReader();
+//         reader.readAsDataURL(selectedFile);
+//         reader.onloadend = (event: any) => {
+//           setPdfFile(event.target.result);
+//           setPdfFileError("");
+//         };
+
+//         // Call the API to upload the training data
+//         const formData = new FormData();
+//         formData.append("file", selectedFile);
+//         formData.append(
+//           "req",
+//           JSON.stringify({
+//             username: "esther",
+//             chatbotID: "chatbot1",
+//           })
+//         );
+
+//         fetch("http://127.0.0.1:8000/api/chatbot/uploadTrainingData", {
+//           method: "POST",
+//           body: formData,
+//         })
+//           .then((response) => response.json())
+//           .then((data) => {
+//             console.log("File uploaded successfully:", data.filename);
+//           })
+//           .catch((error) => {
+//             console.error("Error uploading file:", error);
+//           });
+//         setSelectedFile(selectedFile);
+//       } else {
+//         setPdfFile(null);
+//         setPdfFileError("Please select valid PDF File.");
+//       }
+//     } else {
+//       setPdfFileError("Please select your file.");
+//     }
+//   };
+
+//   // function to handle view PDF
+//   const handlePDFFileViewer = (e: any) => {
+//     e.preventDefault();
+//     if (pdfFile !== null) {
+//       setViewPdf(pdfFile);
+//     } else {
+//       setViewPdf(null);
+//       setPdfFileError("Please select your file.");
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div>
+//         <div className="font-work-sans font-semibold text-[25px] px-4 md:text-[45px] md:px-0 text-center pt-[40px]">
+//           Say Hello To{" "}
+//           <TextLinearGradient brand> Quick, Accurate </TextLinearGradient>{" "}
+//           support
+//         </div>
+//         <div className="font-work-sans font-medium text-[20px] px-4 md:text-[35px] text-center pt-[10px]">
+//           Make Your Own Chatbot Here
+//         </div>
+//       </div>
+
+//       {/* PDF file drag and drop here */}
+
+//       <div>
+//         <div className="flex flex-col justify-center items-center mt-[30px] md:mt-[50px]">
+//           <div className="font-work-sans font-semibold text-xl md:text-3xl mb-1 mr-[216px] md:mr-[460px]">
+//             PDF File
+//           </div>
+//           <FileUploader handlePDFFile={handlePDFFile}/>
+
+//           {/* display selected file with option to remove and choose another */}
+//           {selectedFile && (
+//             <div className="mt-[10px] ml-[25px] file-preview flex">
+//               <div className="flex mb-[10px] p-[15px] rounded-md bg-[#F2F2F2] md:w-[600px] relative file-preview__item">
+//                 <div className="w-[50px] h-[50px] flex items-center pr-[15px]">
+//                   <FileIcon extension="pdf" {...defaultStyles.pdf} />
+//                 </div>
+//                 <div className="file-preview__item__info">
+//                   <p>{selectedFile.name}</p>
+//                   <p>{selectedFile.size}</p>
+//                 </div>
+//                 <button
+//                   style={{ borderRadius: "50%" }}
+//                   className=" text-white text-center bg-red-500 w-6 h-6 absolute right-[20px] top-1/2 transform -translate-y-1/2 flex items-center justify-center file-preview__item__del"
+//                   onClick={() => setSelectedFile(null)}
+//                 >
+//                   x
+//                 </button>
+//               </div>
+//             </div>
+//           )}
+
+//           <button
+//             type="button"
+//             style={{ transition: "all .3s cubic-bezier(0,0,.5,1)" }}
+//             className=" w-[80px] md:w-[100px] mr-[216px] md:mr-[475px] my-[10px] mb-[50px] text-center font-work-sans text-[#193338] bg-brand-sunglow font-medium rounded-lg text-sm px-3 py-2.5 uploadBtn"
+//             onClick={(e) => {
+//               // handlePDFFile(e);
+//               handlePDFFileViewer(e);
+//             }}
+//           >
+//             Upload
+//           </button>
+
+//           {pdfFileError && (
+//             <div className="error-msg text-red-500 text-[16px]">
+//               {pdfFileError}
+//             </div>
+//           )}
+//         </div>
+
+//         <br></br>
+
+//         <div className="flex flex-col">
+//           <div className="font-work-sans font-semibold text-[30px] flex w-auto mt-[50px] ml-[215px]">
+//             Page Preview
+//           </div>
+//           <div className="w-[1000px] h-[800px] bg-[#e4e4e4] flex justify-center items-center overflow-y-auto mx-auto px-4 my-[20px] pdf-container">
+//             {viewPdf && (
+//               <>
+//                 <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.8.162/build/pdf.worker.min.js">
+//                   <Viewer
+//                     fileUrl={viewPdf}
+//                     plugins={[defaultLayoutPluginInstance]}
+//                   />
+//                 </Worker>
+//               </>
+//             )}
+//           </div>
+//           <button
+//             type="button"
+//             style={{ transition: "all .3s cubic-bezier(0,0,.5,1)" }}
+//             className="text-white bg-brand-persian-green focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center w-[80px] md:w-[100px] ml-[215px] mb-[50px] confirm_btn"
+//           >
+//             Confirm
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// test for drag and drop
 export const ChatbotPage = () => {
   // create new plugin instance
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
@@ -26,19 +199,34 @@ export const ChatbotPage = () => {
   // view PDF
   const [viewPdf, setViewPdf] = useState(null);
 
+  // for displaying the selected pdf name and size below with option to delete
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   const fileType = ["application/pdf"];
 
-  const handlePDFFile = (e: any) => {
-    let selectedFile = e.target.files && e.target.files[0];
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // for drag and drop functionality
+  const handleDragOver = (event: any) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event: any) => {
+    event.preventDefault();
+    handlePDFFile(event.dataTransfer.files[0]);
+  };
+
+  const handlePDFFile = (event: any) => {
+    event.preventDefault();
+    console.log(event);
+    let selectedFile = event.target.files && event.target.files[0];
     // if the person has even selected a file
     if (selectedFile) {
       if (selectedFile && fileType.includes(selectedFile.type)) {
         let reader = new FileReader();
         reader.readAsDataURL(selectedFile);
-        reader.onloadend = (e: any) => {
-          setPdfFile(e.target.result);
+        reader.onloadend = (event: any) => {
+          setPdfFile(event.target.result);
           setPdfFileError("");
         };
 
@@ -53,13 +241,13 @@ export const ChatbotPage = () => {
           })
         );
 
-        axios
-          .post(
-            "http://127.0.0.1:8000/api/chatbot/uploadTrainingData",
-            formData
-          )
-          .then((response) => {
-            console.log("File uploaded successfully:", response.data.filename);
+        fetch("http://127.0.0.1:8000/api/chatbot/uploadTrainingData", {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("File uploaded successfully:", data.filename);
           })
           .catch((error) => {
             console.error("Error uploading file:", error);
@@ -105,7 +293,11 @@ export const ChatbotPage = () => {
           <div className="font-work-sans font-semibold text-xl md:text-3xl mb-1 mr-[216px] md:mr-[460px]">
             PDF File
           </div>
-          <FileUploader handlePDFFile={handlePDFFile} />
+          <FileUploader
+            handlePDFFile={handlePDFFile}
+            handleDragOver={handleDragOver}
+            handleDrop={handleDrop}
+          />
 
           {/* display selected file with option to remove and choose another */}
           {selectedFile && (
@@ -121,7 +313,10 @@ export const ChatbotPage = () => {
                 <button
                   style={{ borderRadius: "50%" }}
                   className=" text-white text-center bg-red-500 w-6 h-6 absolute right-[20px] top-1/2 transform -translate-y-1/2 flex items-center justify-center file-preview__item__del"
-                  onClick={() => setSelectedFile(null)}
+                  onClick={() => {
+                    setSelectedFile(null);
+                    setPdfFile(null);
+                  }}
                 >
                   x
                 </button>
@@ -157,7 +352,7 @@ export const ChatbotPage = () => {
           <div className="w-[1000px] h-[800px] bg-[#e4e4e4] flex justify-center items-center overflow-y-auto mx-auto px-4 my-[20px] pdf-container">
             {viewPdf && (
               <>
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.8.162/build/pdf.worker.min.js">
                   <Viewer
                     fileUrl={viewPdf}
                     plugins={[defaultLayoutPluginInstance]}
@@ -170,9 +365,15 @@ export const ChatbotPage = () => {
             type="button"
             style={{ transition: "all .3s cubic-bezier(0,0,.5,1)" }}
             className="text-white bg-brand-persian-green focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center w-[80px] md:w-[100px] ml-[215px] mb-[50px] confirm_btn"
+            onClick={() => setIsModalOpen(true)}
           >
             Confirm
           </button>
+          {isModalOpen && (
+            <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+              <ChatbotTraining setIsModalOpen={setIsModalOpen} />
+            </div>
+          )}
         </div>
       </div>
     </>
