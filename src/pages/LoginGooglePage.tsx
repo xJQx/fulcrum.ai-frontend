5c29ce2249f55d95ab9e585d3734c00a62416f27
Jing Qiang <tohjingqiang@gmail.com>
2023-08-01 23:15:13 -0700

    update user login flow to match backend

--- src/pages/LoginGooglePage.tsx
+++ src/pages/LoginGooglePage.tsx
@@ -6,37 +6,76 @@ import jwt from "jwt-decode";
 import toast from "react-hot-toast";
 import { clientBaseUrl } from "config/client";
 import { getGoogleAuthResult, loginWithGoogle } from "db/firebase";
+import useFetch from "hooks/useFetch";
+import { JwtUserSchema } from "schemas/jwt";
 
 export const LoginGooglePage = () => {
   const navigate = useNavigate();
   const authContext = useContext(AuthContext);
+  const fetch = useFetch();
 
   // Firebase Google OAuth
   const handleLogIn = async () => {
-    console.log("handleLogIn");
     await loginWithGoogle();
   };
 
-  useEffect(() => {
-    const getLogInResult = async () => {
-      console.log("getLogInResult");
-      const response = await getGoogleAuthResult();
-      if (!response) {
-        return;
-      } else {
-        console.log(response);
-        const { token, user } = response;
-        authContext.setAccessToken(token);
-        authContext.setUser(user);
-        authContext.setIsLoggedIn(true);
-        navigate(`${clientBaseUrl}dashboard`);
+  /**
+   * Parse Firebase Google OAuth Login Results.
+   * Includes registering users in the backend and setting the authorization headers and global Auth State
+   */
+  const parseLogInResult = async () => {
+    const response = await getGoogleAuthResult();
+    if (!response) {
+      return;
+    } else {
+      console.log(response);
+      const { token, user } = response;
+      console.log("token:", token);
+      console.log("user:", user);
+
+      // Register User if User does not exist in the server.
+      const { userExists } = await fetch.get(
+        `/user/checkExist/userid/${user.id}` // TODO: this should be firebase user_id
+      );
+      if (!userExists) {
+        const formData = new FormData();
+        formData.append("userid", user.id); // TODO: this should be firebase user_id
+        formData.append("name", user.name); // TODO: this should be firebase name
+        formData.append("email", user.email); // TODO: this should be firebase email
+        const res = await fetch.post("/user/register", formData, "form");
+        if (res.user) {
+          // user created succesfully
+          toast.success("User has been successfully registered");
+        } else {
+          console.log(res.error);
+          toast.error("Failed to register user");
+          return;
+        }
       }
-    };
+
+      // Update State
+      const loggedInUser: JwtUserSchema = {
+        id: user.id, // TODO: this should be firebase user_id
+        email: user.email, // TODO: this should be firebase email
+        name: user.name, // TODO: this should be firebase name
+        registered: true,
+      };
+      authContext.setAccessToken(token);
+      authContext.setUser(loggedInUser);
+      authContext.setIsLoggedIn(true);
+
+      toast.success("Log in successfully.");
+      navigate(`${clientBaseUrl}dashboard`);
+    }
+  };
+
+  useEffect(() => {
     if (authContext.isLoggedIn) {
-      toast.success("Logged In Successfully. Welcome " + "user");
+      toast.success("Logged In Successfully. Welcome " + authContext.user.name);
       navigate(clientBaseUrl);
     } else {
-      getLogInResult();
+      // Log in Results will only be parsed when user is redirected to this page back from the Google OAuth Log in
+      parseLogInResult();
     }
   }, []);
 
