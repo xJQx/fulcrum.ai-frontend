7d60420cd42e6d82947493dbf512b711fdea77a6
estherteo <104299126+estherteo@users.noreply.github.com>
2023-08-09 18:26:48 +0800

    UI updates for chat page

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -31,6 +31,13 @@ const ChatPage = () => {
     setIsTyping(false);
   };
 
+  const handleEnter = (event: any) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleSendMessage();
+    }
+  };
+
   // handles user sending their question for chatbot to answer
   const handleSendMessage = () => {
     if (message.trim() !== "") {
@@ -81,27 +88,18 @@ const ChatPage = () => {
 
   return (
     <div className="h-full flex">
-      <div className="bg-white h-full flex flex-col flex-1 chatbot w-4/6">
-        <div className="bg-gray-50 border-b border-black/10 w-full chatbot">
-          <div className="flex items-center p-[18px] md:p-[24px] m-auto gap-4 w-5/6 md:w-4/6">
-            <div className="flex items-center justify-center rounded-full bg-brand-sunglow h-10 w-10 md:h-12 md:w-12 avatar">
-              <img
-                src="/fulcrum.ai-frontend/assets/fulcrum.ai_logo.png"
-                alt="Fulcrum.ai Logo"
-                className="h-8 w-8 md:h-10 md:w-10"
-              />
-            </div>
-            <div className="text-black chat-message">{chatbotResponse}</div>
-          </div>
-        </div>
-
+      <div className="bg-white h-full flex flex-col flex-1 chatbot w-full">
         {/* Chat messages section */}
         <div className="w-full user">
           {conversationHistory.map((msg, index) => (
             <div
               key={index}
               className={`flex items-center p-[18px] md:p-[24px] m-auto gap-4 w-5/6 md:w-4/6 border-b border-black/10 ${
-                msg.type === "user" ? "" : "bg-white"
+                msg.type === "user"
+                  ? "bg-white"
+                  : msg.type === "chatbot"
+                  ? "bg-gray-50"
+                  : ""
               }`}
             >
               {msg.type === "user" && (
@@ -111,6 +109,17 @@ const ChatPage = () => {
                   </span>
                 </div>
               )}
+
+              {msg.type === "chatbot" && (
+                <div className="rounded-full bg-brand-sunglow h-10 w-10 md:h-12 md:w-12 flex justify-center items-center avatar">
+                  <img
+                    src="/fulcrum.ai-frontend/assets/fulcrum.ai_logo.png"
+                    alt="Fulcrum.ai Logo"
+                    className="h-8 w-8 md:h-10 md:w-10"
+                  />
+                </div>
+              )}
+
               <div
                 className={`text-black chat-message ${
                   msg.type === "user" ? "user" : "chatbot"
@@ -121,6 +130,7 @@ const ChatPage = () => {
             </div>
           ))}
         </div>
+        <div className="bg-gray-50 w-full"></div>
 
         {/* Text input field for the question */}
         <div className="fixed bottom-0 w-full flex items-end justify-center chat-input bg-white">
@@ -133,6 +143,7 @@ const ChatPage = () => {
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               onBlur={handleBlur}
+              onKeyDown={handleEnter}
             ></textarea>
             <button
               className={`absolute right-4 top-1/2 transform -translate-y-1/2 mt-auto flex items-center justify-center ${
